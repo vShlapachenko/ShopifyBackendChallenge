74e18e26c5e1dcfc80cce0db06f17e9208f80976
vShlapachenko <valentyn.shlapachenko@gmail.com>
2021-09-09 10:21:23 -0700

    initial commit

---
+++ src/main/java/com/github/vshlapachenko/shopify/backend/controllers/ImageController.java
@@ -0,0 +1,4 @@
+package com.github.vshlapachenko.shopify.backend.controllers;
+
+public class ImageController {
+}
---
+++ src/test/java/com/github/vshlapachenko/shopify/backend/ShopifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vshlapachenko.shopify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
